dbb1a66bbf19d28a8939f9d490ec556e1a2f6b45
hackhubs <abhav.thakur2018@vitstudent.ac.in>
2020-12-28 02:13:41 +0530

    AppText

--- app/components/ItemPicker.js
+++ app/components/ItemPicker.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 const ItemPicker = () => {
   const [selectedValue, setSelectedValue] = useState('java');
   return (
     <View style={styles.container}>
+      <MaterialCommunityIcons name="apps" size={25} />
       <Picker
         selectedValue={selectedValue}
         style={{ height: 50, width: 150 }}
@@ -22,6 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 40,
+    flexDirection: 'row',
     alignItems: 'center',
   },
 });
